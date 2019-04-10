e3be397f02e07ea538d74a34493920a95b1173be
loavila <48717912+loavila@users.noreply.github.com>
2019-04-10 11:11:38 -0300

    Update __init__.py

--- gym_mppt/__init__.py
+++ gym_mppt/__init__.py
@@ -1,1 +1,11 @@
+from gym.envs.registration import register
 
+register(
+    id='mppt-v0',
+    entry_point='gym_mppt.envs:MpptEnv',
+)
+''' register(
+    id='foo-extrahard-v0',
+    entry_point='gym_foo.envs:FooExtraHardEnv',
+)
+'''
