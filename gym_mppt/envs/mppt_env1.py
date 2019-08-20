f9b91f69949112a14db4d84a51e03494977c5aa6
marianodepaula <mariano.depaula@fio.unicen.edu.ar>
2019-08-20 11:34:09 -0300

    arreglado

--- gym_mppt/envs/mppt_env1.py
+++ gym_mppt/envs/mppt_env1.py
@@ -140,7 +140,9 @@ class MpptEnv1(gym.Env):
         
         #irradiancias = list([100., 200., 300., 400., 500., 600., 700., 800., 900., 1000])
         #temperaturas = list([13.5, 15., 17.5, 20., 22.5, 25., 27.5, 30., 32.5, 35])
-       return self.state
+        self.Temp = 25#random.sample(temperaturas,1)[0] #(Elegir un random de estos) o dejar fija la T y solo variar la irr pa empezar a probar...
+        self.Irr = 100#random.sample(irradiancias, 1)[0] #random.sample(irradiancias,1) # [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0] (Elegir un random de estos)
+        return self.state
 
     '''
     def setTempIrr(self,last_state,T,G):
